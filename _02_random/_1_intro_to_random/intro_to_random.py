78473e387133f6bc0dcd0628358ec9abe940fa8c
league <karinabriellen@gmail.com>
2024-01-17 16:41:40 -0800

    will be back

--- _02_random/_1_intro_to_random/intro_to_random.py
+++ _02_random/_1_intro_to_random/intro_to_random.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     # Prints out 5 random whole numbers between 0 and 100 (0 and 100 both included)
     for i in range(5):
-        number = random.randint(0, 100)
+        number = random.randint(-50, 5)
         print(number)
 
     # TODO Print out 5 random numbers between -50 and 5
@@ -31,5 +31,7 @@ if __name__ == '__main__':
 
     # TODO Print out 5 random decimal numbers between -123.45 and 67.89
     for i in range(5):
+        number = random.uniform(-123.45, 67.89)
+        print(number)
 
         pass
